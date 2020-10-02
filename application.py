94d46f2e107dcabe0e4d66ba317748280246afe1
Robert Stolz <rstolz@ucdavis.edu>
2020-10-02 10:45:48 -0700

    Initial commit.

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+    trades = {}
+    with open("static/test_trades.json",'r') as test_trades:
+        trades = json.load(test_trades)
+        trades.sort(reverse=True, key=lambda t : t['ppj'])
+    #sprites = [json['sprites'][key] for key in json['sprites'] if isinstance(json['sprites'][key], str)]
+    return render_template('hello.html',trades=trades)
+
+if __name__ == '__main__':
+    app.run()
