d829ed6f2b53364bea6fca504a7fa2374e99b23b
Robert Stolz <rstolz@ucdavis.edu>
2020-10-12 23:07:41 -0700

    updated flask to serve as my API endpoint for trades.

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
-import requests
 import json
+import redis
+
+r = redis.Redis(host='ec2-54-215-48-242.us-west-1.compute.amazonaws.com', port=6379, db=2, password='insight')
 
 app = Flask(__name__)
 
-
-
+'''
 @app.route('/')
 def hello_world():
     trades = {}
@@ -14,6 +15,20 @@ def hello_world():
         trades.sort(reverse=True, key=lambda t : t['ppj'])
     #sprites = [json['sprites'][key] for key in json['sprites'] if isinstance(json['sprites'][key], str)]
     return render_template('hello.html',trades=trades)
+'''
+
+@app.route('/trades')
+def serve_trades():
+    trades = {}
+    current_iter = 0
+    trades[current_iter] = json.loads(r.get(current_iter))
+    while True:
+        current_iter, keys = r.scan(current_iter)
+        for key in keys:
+            trades[key] = json.loads(r.get(key))
+        if current_iter == 0:
+            break
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
